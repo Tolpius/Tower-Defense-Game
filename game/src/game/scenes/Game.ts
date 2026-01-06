7ed2a77a8ca470a1e09e5e1c94309468cbbf698c
Tolpius <167006924+Tolpius@users.noreply.github.com>
2026-01-06 12:37:40 +0100

    bugfix: removed some Type errors

--- game/src/game/scenes/Game.ts
+++ game/src/game/scenes/Game.ts
@@ -1,6 +1,5 @@
 import { Scene } from "phaser";
 export class Game extends Scene {
-
     constructor() {
         super("Game");
     }
@@ -10,27 +9,38 @@ export class Game extends Scene {
             key: "mapOne",
         });
         const tilesetGrass = map.addTilesetImage("GrassTileset", "grass");
-        const tilesetWater = map.addTilesetImage(
-            "AnimatedWaterTiles",
-            "water"
-        );
+        const tilesetWater = map.addTilesetImage("AnimatedWaterTiles", "water");
 
-        const layerBackground = map.createLayer(
-            "Terrain_Background",
-            tilesetGrass,
-            0,
-            0
-        );
-        const layerPath = map.createLayer("Terrain_Path", tilesetGrass, 0, 0);
-        const layerWater = map.createLayer("Terrain_Water", tilesetWater, 0, 0);
-        const layerCliffs = map.createLayer(
-            "Terrain_Cliffs",
-            tilesetGrass,
-            0,
-            0
-        );
-        const layerProps = map.createLayer("Props", tilesetGrass, 0, 0);
-        const layerDetails = map.createLayer("Details", tilesetGrass, 0, 0);
+        if (tilesetGrass) {
+            const layerBackground = map.createLayer(
+                "Terrain_Background",
+                tilesetGrass,
+                0,
+                0
+            );
+            const layerPath = map.createLayer(
+                "Terrain_Path",
+                tilesetGrass,
+                0,
+                0
+            );
+            const layerCliffs = map.createLayer(
+                "Terrain_Cliffs",
+                tilesetGrass,
+                0,
+                0
+            );
+            const layerProps = map.createLayer("Props", tilesetGrass, 0, 0);
+            const layerDetails = map.createLayer("Details", tilesetGrass, 0, 0);
+        }
+        if (tilesetWater) {
+            const layerWater = map.createLayer(
+                "Terrain_Water",
+                tilesetWater,
+                0,
+                0
+            );
+        }
     }
 }
 

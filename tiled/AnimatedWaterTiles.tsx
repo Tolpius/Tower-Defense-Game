a0d7e6c47c79e0ced543dc83b2c77778d102b7b5
dominikmusewitsch <dmusewitsch@gmail.com>
2026-01-06 11:23:08 +0100

    repair tiled map

--- tiled/AnimatedWaterTiles.tsx
+++ tiled/AnimatedWaterTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="AnimatedWaterTiles" tilewidth="64" tileheight="64" tilecount="490" columns="70">
- <image source="../Downloads/Foozle_2DT0012_Spire_Tileset_1/Tileset/PNGs/Animated water tiles.png" width="4480" height="448"/>
+ <image source="Foozle_2DT0012_Spire_Tileset_1/Tileset/PNGs/Animated water tiles.png" width="4480" height="448"/>
 </tileset>

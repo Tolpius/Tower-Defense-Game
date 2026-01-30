c73d03b2b31b19297ef05ae6eff075fc617589d8
dominikmusewitsch <dmusewitsch@gmail.com>
2026-01-30 11:53:07 +0100

    redo level selection and add level preview

--- game/src/game/scripts/preloader/worldAssets.ts
+++ game/src/game/scripts/preloader/worldAssets.ts
@@ -6,6 +6,12 @@ export default function loadWorldAssets(preloader: Preloader) {
     preloader.load.image("background", "/assets/background.png");
     preloader.load.image("enemy", "star.png");
     preloader.load.image("td-map-lvl1", "/map/TD-map-lvl1.png");
+    preloader.load.image("map-preview-1", "/map/TD-map-lvl1.png");
+    preloader.load.image("map-preview-2", "/map/TD-map-lvl2.png");
+    preloader.load.image("map-preview-3", "/map/TD-map-lvl3.png");
+    preloader.load.image("map-preview-4", "/map/TD-map-lvl4.png");
+    preloader.load.image("map-preview-5", "/map/TD-map-lvl5.png");
+    preloader.load.image("map-preview-6", "/map/TD-map-lvl6.png");
     preloader.load.tilemapTiledJSON("mapOne", "/map/TD-map-lvl1.json");
     preloader.load.image("solidGreen", "/Solid_green.png");
     preloader.load.image("grass", "/tilesets/GrassTileset.png");
@@ -18,4 +24,3 @@ export default function loadWorldAssets(preloader: Preloader) {
         frameHeight: 64,
     });
 }
-

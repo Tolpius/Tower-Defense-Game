5967c1a27bf9f54103f4492726216486b4a528c8
dominikmusewitsch <dmusewitsch@gmail.com>
2026-02-02 15:11:17 +0100

    add levels to world 3

---
+++ game/public/assets/tilesets/AnimatedWaterWinter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="AnimatedWaterWinter" tilewidth="64" tileheight="64" tilecount="490" columns="70">
+ <image source="AnimatedWaterWinter.png" width="4480" height="448"/>
+</tileset>
